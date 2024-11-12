95c9e39b975799f1f669c8510a92bb3893cd4df8
rafael <146065062+rafaelsanchezt@users.noreply.github.com>
2024-11-12 02:20:54 -0300

    Add files via upload

---
+++ PublicacionesyComentarios/src/test/java/com/example/PublicacionesyComentarios/PublicacionesyComentariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PublicacionesyComentarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicacionesyComentariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
