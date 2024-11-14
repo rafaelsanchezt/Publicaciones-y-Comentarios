48e325d803c9fa4697e7da8555fbdb870ef55263
BousoGonzalo <gonzalobouso2@gmail.com>
2024-11-14 02:22:56 -0300

    Fix Post

--- PublicacionesyComentarios/src/main/java/com/example/PublicacionesyComentarios/PublicacionesyComentariosApplication.java
+++ PublicacionesyComentarios/src/main/java/com/example/PublicacionesyComentarios/PublicacionesyComentariosApplication.java
@@ -1,9 +1,11 @@
 package com.example.PublicacionesyComentarios;
-
-import org.springframework.boot.SpringApplication;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
 
 @SpringBootApplication
+@EnableReactiveMongoRepositories(basePackages = "com.example.PublicacionesyComentarios.repository")
 public class PublicacionesyComentariosApplication {
     public static void main(String[] args) {
         SpringApplication.run(PublicacionesyComentariosApplication.class, args);
